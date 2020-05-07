75bfc508c127657d78d0589ab0f9dfe5d1b7c50d
harmonious-cable-1188 <rtyenriques@gmail.com>
2020-05-07 22:27:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,10 +1,4 @@
-def favorite_color(color)
-  puts "Your favorie color is #{color}"
+def introuduction(name)
+  puts "Hi, my name is #{name}"
 end
-favorite_color("red")
-
-
-def greeting_married_couple(husband,wife)
-  puts "Congrats #{husband} and #{wife} on tying the knot!"
-end
-  greeting_married_couple("Ross","Chelssie")
+introuduction("Josh")
